6e1865ebb7c402ff6c7760b2900fd2f970e27059
memory98 <asdlkjh252567@gmail.com>
2023-03-03 13:58:24 +0900

    package

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,10 @@
+/**
+ * douzone-math 파일 모듈 테스트
+ * 
+ */
+
+var math = require('../douzone-math/index');
+
+console.log(math.PI);
+console.log(math.min(150,200,30,40,50));
+console.log(math.max(150,200,30,40,50));
